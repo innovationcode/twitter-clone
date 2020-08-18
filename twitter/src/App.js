7a760b9b2924329204acf3a21b1dfb83115056fe
lajawanti <lajawanti.dhake@gmail.com>
2020-08-18 13:54:57 -0400

    structure defined

--- twitter/src/App.js
+++ twitter/src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="app">
       <h1>Twitter-Clone.......</h1>
+      {/* Sidebar */}
+      {/* Feed */}
+      {/* Widgets */}
     </div>
   );
 }
---
+++ twitter/src/Components/sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar = () => {
+    return(
+        <div className="sidebar">
+            {/* Twitter icon */}
+
+            {/* Sidebar options */}
+
+            {/* Button - Tweet */}
+        </div>
+    )
+}
+
+export default Sidebar;
